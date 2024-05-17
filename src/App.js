49b38fec50ad0751e40479349598d68a54d3e58f
karthikeya reddy vasipalli <reddyvasipalli98@outlook.com>
2024-05-17 10:40:11 -0400

    added header changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Header from './components/Header';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  return <div>
+    <Header />
+  </div>;
 }
 
 export default App;
